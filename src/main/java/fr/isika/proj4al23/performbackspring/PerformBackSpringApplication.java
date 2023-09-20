c5b6dd54a1ad8de2385405764bd480b773622b77
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-21 00:02:15 +0200

    first commit

---
+++ src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
@@ -0,0 +1,13 @@
+package fr.isika.proj4al23.performbackspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerformBackSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerformBackSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.isika.proj4al23.performbackspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerformBackSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
