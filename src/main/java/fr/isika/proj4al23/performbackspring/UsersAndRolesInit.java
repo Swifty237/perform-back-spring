e1cab89111390978cf7e72e5efeed1d3cb777bae
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-22 10:10:14 +0200

    add class initialization database

---
+++ src/main/java/fr/isika/proj4al23/performbackspring/UsersAndRolesInit.java
@@ -0,0 +1,55 @@
+package fr.isika.proj4al23.performbackspring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import fr.isika.proj4al23.performbackspring.models.PerformRole;
+import fr.isika.proj4al23.performbackspring.models.PerformUser;
+import fr.isika.proj4al23.performbackspring.service.AccountService;
+
+@Component
+public class UsersAndRolesInit implements ApplicationRunner{
+
+	@Autowired
+	public AccountService accountService;
+	
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+	
+//		PerformRole roleUser = new PerformRole();
+//		roleUser.setRoleName("USER");
+//		
+//		PerformRole roleAdmin = new PerformRole();
+//		roleUser.setRoleName("ADMIN");
+//		
+//		PerformUser user1 = new PerformUser();
+//		user1.setEmail("user1@example.com");
+//		user1.setUsername("user1");
+//		user1.setPassword("1234");
+//		
+//		PerformUser user2 = new PerformUser();
+//		user2.setEmail("user2@example.com");
+//		user2.setUsername("user2");
+//		user2.setPassword("5678");
+//		
+//		PerformUser user3 = new PerformUser();
+//		user3.setEmail("user3@example.com");
+//		user3.setUsername("user3");
+//		user3.setPassword("9012");
+//		
+//		accountService.addNewRole(roleUser);
+//		accountService.addNewRole(roleAdmin);
+//			
+//		accountService.addNewUser(user1);
+//		accountService.addNewUser(user2);
+//		accountService.addNewUser(user3);
+//			
+//		accountService.addRoleToUser("user1", "USER");
+//		accountService.addRoleToUser("user1", "ADMIN");
+//			
+//		accountService.addRoleToUser("user2", "USER");
+//		accountService.addRoleToUser("user3", "USER");
+	}
+}
