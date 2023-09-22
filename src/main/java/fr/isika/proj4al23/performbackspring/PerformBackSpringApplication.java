c64c41ca0bcfc01887e96ca2902257b220d11508
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-22 10:07:57 +0200

    update commit

--- src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
+++ src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PerformBackSpringApplication {
-
+public class PerformBackSpringApplication  {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PerformBackSpringApplication.class, args);
 	}
-
 }
--- src/main/java/fr/isika/proj4al23/performbackspring/service/AccountServiceImpl.java
+++ src/main/java/fr/isika/proj4al23/performbackspring/service/AccountServiceImpl.java
@@ -34,8 +34,8 @@ public class AccountServiceImpl implements AccountService {
 
 	@Override
 	public void addRoleToUser(String username, String roleName) {
-		PerformUser performUser = this.performUserRepository.findByUsername(username);
-		PerformRole performRole = this.performRoleRepository.findByRoleName(roleName);
+		PerformUser performUser = performUserRepository.findByUsername(username);
+		PerformRole performRole = performRoleRepository.findByRoleName(roleName);
 		performUser.getRoles().add(performRole);
 	}
 
