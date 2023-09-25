f0933028047b76bfdfe6651239d94b7c08819dd3
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-26 01:22:14 +0200

    adding encryption class in the context of the application

--- src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
+++ src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
@@ -2,11 +2,19 @@ package fr.isika.proj4al23.performbackspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class PerformBackSpringApplication  {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PerformBackSpringApplication.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
