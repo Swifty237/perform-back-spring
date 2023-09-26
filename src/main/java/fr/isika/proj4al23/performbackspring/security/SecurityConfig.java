11f1efb60560b9105c3a168ebbd55ed9183bf9b1
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-26 13:32:13 +0200

    refactoring commit

--- src/main/java/fr/isika/proj4al23/performbackspring/security/SecurityConfig.java
+++ src/main/java/fr/isika/proj4al23/performbackspring/security/SecurityConfig.java
@@ -1,8 +1,5 @@
 package fr.isika.proj4al23.performbackspring.security;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,49 +9,35 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import fr.isika.proj4al23.performbackspring.models.PerformUser;
-import fr.isika.proj4al23.performbackspring.service.AccountService;
+import fr.isika.proj4al23.performbackspring.security.filters.JwtAuthenticationFilter;
+import fr.isika.proj4al23.performbackspring.security.filters.JwtAuthorizationFilter;
+import fr.isika.proj4al23.performbackspring.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
-	private AccountService accountService;
+	private UserDetailsServiceImpl userDetailsService;
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(new UserDetailsService() {
-			
-			@Override
-			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				PerformUser performUser = accountService.loadUserByUsername(username);
-				
-				Collection<GrantedAuthority> authorities = new ArrayList<>();
-				
-				performUser.getPerformRoles().forEach(role -> {
-					authorities.add(new SimpleGrantedAuthority(role.getRoleName()));
-				});
-				
-				
-				return new User(performUser.getUsername(), performUser.getPassword(), authorities);
-			}
-		});
+		auth.userDetailsService(userDetailsService);
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // les cookies de session ne sont plus enregistré
+		http.authorizeRequests().antMatchers("/refreshtoken/**", "/login/**").permitAll();
 		http.authorizeRequests().anyRequest().authenticated();
+//		http.authorizeRequests().antMatchers(HttpMethod.POST, "/user/**").hasAnyAuthority("SUPER ADMIN");
+//		http.authorizeRequests().antMatchers(HttpMethod.POST, "/user/**").hasAnyAuthority("ADMIN");
+//		http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/**").hasAnyAuthority("USER");
 		http.addFilter(new JwtAuthenticationFilter(authenticationManagerBean()));
+		http.addFilterBefore(new JwtAuthorizationFilter(),  UsernamePasswordAuthenticationFilter.class);
 	}
 	
 	@Bean
