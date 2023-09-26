4d0d55708c714ef44cd3f6f48484fe173c4a481f
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-26 13:27:17 +0200

    adding @EnableGlobalMethodSecurity annonation

--- src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
+++ src/main/java/fr/isika/proj4al23/performbackspring/PerformBackSpringApplication.java
@@ -3,10 +3,12 @@ package fr.isika.proj4al23.performbackspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // => pour pouvoir utiliser les annotations
 public class PerformBackSpringApplication  {
 	
 	public static void main(String[] args) {
