31cac5b072fb15c7dd6584051d450f871d00ab35
Yannick Kamdem Kouam <yannickkamdemkouam@yahoo.fr>
2023-09-29 23:10:56 +0200

    Renaming SecurityController and adding register endpoint

--- src/main/java/fr/isika/proj4al23/performbackspring/controller/AuthLoginController.java
+++ src/main/java/fr/isika/proj4al23/performbackspring/controller/AuthLoginController.java
@@ -19,39 +19,57 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fr.isika.proj4al23.performbackspring.models.PerformUser;
+import fr.isika.proj4al23.performbackspring.service.UserAccountServiceImpl;
+
 @RestController
 @RequestMapping("/auth")
 public class AuthLoginController {
 
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
+
+	@Autowired
+	private UserAccountServiceImpl userAccountService;
+
 	@Autowired
 	private JwtEncoder jwtEncoder;
-	
+
 	@GetMapping("/profile")
 	public Authentication authentication(Authentication authentication) {
 		return authentication;
 	}
-	
+
 	@PostMapping("/login")
 	public Map<String, String> login(String username, String password) {
-		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-		
-		Instant instant= Instant.now();
-		String scope = authentication.getAuthorities().stream().map(a -> a.getAuthority()).collect(Collectors.joining(" "));
-		
-		JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder()
-				.issuedAt(instant)
-				.expiresAt(instant.plus(10, ChronoUnit.MINUTES))
-				.subject(username)
-				.claim("scope", scope)
-				.build();
-		
-		JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters.from(JwsHeader.with(MacAlgorithm.HS256).build(), jwtClaimsSet);
-		
-		String jwt  = jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
-		
+		Authentication authentication = authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+		Instant instant = Instant.now();
+		String scope = authentication.getAuthorities().stream().map(a -> a.getAuthority())
+				.collect(Collectors.joining(" "));
+
+		JwtClaimsSet jwtClaimsSet = JwtClaimsSet.builder().issuedAt(instant)
+				.expiresAt(instant.plus(10, ChronoUnit.MINUTES)).subject(username).claim("scope", scope).build();
+
+		JwtEncoderParameters jwtEncoderParameters = JwtEncoderParameters
+				.from(JwsHeader.with(MacAlgorithm.HS256).build(), jwtClaimsSet);
+
+		String jwt = jwtEncoder.encode(jwtEncoderParameters).getTokenValue();
+
 		return Map.of("access-token", jwt);
+	}
+
+	@PostMapping("/register")
+	public Map<String, String> register(String email, String username, String password) {
+		PerformUser performUser = new PerformUser();
+		performUser.setEmail(email);
+		performUser.setUsername(username);
+		performUser.setPassword(password);
+
+		userAccountService.addNewUser(performUser);
+		userAccountService.addRoleToUser(username, "USER");
+
+		return login(username, password);
 	}
 }
